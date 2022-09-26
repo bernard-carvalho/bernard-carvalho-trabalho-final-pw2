b80ceb7483ac6e4201197b945e2a4cd315b1d6e9
bernard-carvalho <bernard.carvalho@ifto.edu.br>
2022-09-26 18:06:36 -0300

    InitialCommit

---
+++ src/main/java/br/edu/ifto/estudante/bernardcarvalhotrabalhofinalpw2/BernardCarvalhoTrabalhoFinalPw2Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifto.estudante.bernardcarvalhotrabalhofinalpw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BernardCarvalhoTrabalhoFinalPw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BernardCarvalhoTrabalhoFinalPw2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifto/estudante/bernardcarvalhotrabalhofinalpw2/BernardCarvalhoTrabalhoFinalPw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifto.estudante.bernardcarvalhotrabalhofinalpw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BernardCarvalhoTrabalhoFinalPw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
